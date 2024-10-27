6e683adb8fe9b0ac8750665f2073f55d69ffc5e8
wiwindaaulia <73329671+wiwindaaulia@users.noreply.github.com>
2024-10-28 06:07:30 +0700

    Update stream-jantung.py

--- stream-jantung.py
+++ stream-jantung.py
@@ -2,9 +2,13 @@ import numpy as np
 import joblib
 import streamlit as st
 
-# Memuat model yang telah dilatih
+# Memuat model yang telah dilatih dengan penanganan kesalahan
 model_filename = 'logistic_regression_model.pkl'
-model = joblib.load(model_filename)
+try:
+    model = joblib.load(model_filename)
+except FileNotFoundError:
+    st.error("Model tidak ditemukan. Pastikan file model ada di direktori yang benar.")
+    st.stop()  # Hentikan eksekusi jika model tidak ditemukan
 
 # Judul aplikasi web
 st.title('Prediksi Penyakit Jantung')
@@ -56,25 +60,27 @@ heart_diagnosis = ''
 
 # Tombol untuk melakukan prediksi
 if st.button('Prediksi Penyakit Jantung'):
+    try:
+        # Mengonversi input ke tipe float
+        heart_prediction = [
+            float(age), float(sex), float(cp), float(trestbps), float(chol), 
+            float(fbs), float(restecg), float(thalach), float(exang), 
+            float(oldpeak), float(slope), float(ca), float(thal)
+        ]
         
-    # Mengonversi input ke tipe float
-    heart_prediction = [
-        float(age), float(sex), float(cp), float(trestbps), float(chol), 
-        float(fbs), float(restecg), float(thalach), float(exang), 
-        float(oldpeak), float(slope), float(ca), float(thal)
-    ]
-    
-    # Mengubah data input menjadi bentuk yang sesuai untuk prediksi tunggal
-    input_data = np.asarray(heart_prediction).reshape(1, -1)
+        # Mengubah data input menjadi bentuk yang sesuai untuk prediksi tunggal
+        input_data = np.asarray(heart_prediction).reshape(1, -1)
 
-    # Melakukan prediksi menggunakan model yang telah dimuat
-    prediction = model.predict(input_data)
+        # Melakukan prediksi menggunakan model yang telah dimuat
+        prediction = model.predict(input_data)
 
-    # Menentukan hasil prediksi
-    if prediction[0] == 0:
-        heart_diagnosis = 'Pasien Tidak Terkena Penyakit Jantung'
-    else:
-        heart_diagnosis = 'Pasien Terkena Penyakit Jantung'
+        # Menentukan hasil prediksi
+        if prediction[0] == 0:
+            heart_diagnosis = 'Pasien Tidak Terkena Penyakit Jantung'
+        else:
+            heart_diagnosis = 'Pasien Terkena Penyakit Jantung'
 
-    # Menampilkan hasil
-    st.success(heart_diagnosis)
+        # Menampilkan hasil
+        st.success(heart_diagnosis)
+    except ValueError:
+        st.error("Terjadi kesalahan dalam memasukkan data. Pastikan semua input sudah benar dan dalam format yang sesuai.")
